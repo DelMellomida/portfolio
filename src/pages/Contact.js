30ac500e8936a4877c14943e774d686921fcae88
DelMellomida <delmellomida@gmail.com>
2024-08-14 15:52:59 +0800

    contact final update

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -29,23 +29,23 @@ function Contact() {
         <React.Fragment>
             <div className="h-screen overflow-auto flex flex-col justify-center items-center">
                 <NavBar />
-                <div className='contactContent mt-24 relative flex flex-wrap items-center justify-end flex-row bg-[#262626] w-[70%] h-fit rounded-2xl p-1 overflow-hidden'>
+                <div className='contactContent mt-24 relative flex flex-col md:flex-row items-center justify-end bg-[#262626] w-[90%] md:w-[70%] h-fit rounded-2xl p-4 md:p-1 overflow-hidden'>
                     <div
-                        className='contact-image absolute inset-0 bg-cover bg-center w-[50%] h-full rounded-2xl z-50'
+                        className='hidden md:flex contact-image absolute inset-0 bg-cover bg-center w-full md:w-[50%] h-56 md:h-full rounded-2xl z-50'
                         style={{ backgroundImage: `url(/contact-bg.webp)` }}
                         alt="contact"
                     >
                     </div>
                     <div className='contact-content relative w-full md:w-1/2 p-6 z-20'>
                         <div className="text-center">
-                            <h1 className="text-4xl font-bold text-slate-200 mb-4">Let's Get in Touch</h1>
+                            <h1 className="text-3xl md:text-4xl font-bold text-slate-200 mb-4">Let's Get in Touch</h1>
                             <p className="text-gray-600 mb-8">
                                 I'm always open to discussing new projects, creative ideas, or opportunities to be part of your visions.
                             </p>
                         </div>
-                        <form onSubmit={handleSubmit} className="w-full max-w-lg bg-[#262626] shadow-md rounded-lg">
-                            <div className="flex flex-wrap justify-between">
-                                <div className="w-full md:w-[48%]">
+                        <form onSubmit={handleSubmit} className="w-full bg-[#262626] flex flex-col justify-center shadow-md rounded-lg">
+                            <div className="flex flex-col md:flex-row justify-between">
+                                <div className="w-full md:w-[48%] mb-4 md:mb-0">
                                     <CustomInput
                                         type="text"
                                         name="name"
@@ -86,9 +86,9 @@ function Contact() {
                                 Send Message
                             </button>
                         </form>
-                        <div className='flex flex-wrap flex-row items-center justify-between'>
+                        <div className='others flex flex-col md:flex-row items-center justify-center md:justify-between mt-4'>
                             <Socials />
-                            <div className="flex justify-center mt-4">
+                            <div className="flex justify-center mt-4 md:mt-0 md:ml-4">
                                 <a href={resumeLink} target="_blank" rel="noopener noreferrer" className="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600 transition duration-300">
                                     View My Resume
                                 </a>
